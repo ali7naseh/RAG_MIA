6f2d9cd5b8d2ef88d8e278bd9a84438ebe3a098c
YF <yfp16443@gmail.com>
2024-10-26 19:45:54 +0000

    new commit

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -3,20 +3,21 @@ import os
 import json
 import random
 
+num=1000
 
-datasets = ['nfcorpus',
-            'trec-covid',
-            'fiqa'
-            ]
+# datasets = ['nfcorpus',
+#             'trec-covid',
+#             'fiqa'
+#             ]
 
-for dataset in datasets:
-    url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
-    out_dir = os.path.join(os.getcwd(), "datasets")
-    data_path = os.path.join(out_dir, dataset)
-    if not os.path.exists(data_path):
-        data_path = util.download_and_unzip(url, out_dir)
+# for dataset in datasets:
+#     url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
+#     out_dir = os.path.join(os.getcwd(), "datasets")
+#     data_path = os.path.join(out_dir, dataset)
+#     if not os.path.exists(data_path):
+#         data_path = util.download_and_unzip(url, out_dir)
 
-os.system('rm datasets/*.zip')
+# os.system('rm datasets/*.zip')
 
 # Function to randomly select 200 mem and 200 non-mem indices from a JSONL dataset
 def select_mem_non_mem_indices(dataset_path):
@@ -30,8 +31,8 @@ def select_mem_non_mem_indices(dataset_path):
             doc_ids.append(doc['_id'])  # Assuming the ID is stored in the '_id' field
     
     random.shuffle(doc_ids)
-    mem_indices = random.sample(doc_ids, 1000)
-    non_mem_indices = random.sample(doc_ids, 1000)
+    mem_indices = random.sample(doc_ids, num)
+    non_mem_indices = random.sample(doc_ids, num)
     
     return mem_indices, non_mem_indices
 
@@ -45,4 +46,4 @@ for dataset in datasets:
     with open(output_file, "w") as f:
         json.dump({"mem_indices": mem_indices, "non_mem_indices": non_mem_indices}, f, indent=4)
 
-    print(f"Saved 200 mem and 200 non-mem indices for {dataset} in {output_file}")
+    print(f"Saved {num} mem and {num} non-mem indices for {dataset} in {output_file}")
